adada54ed46d8840a52066821da74327fd1af8bb
Jimi <jimijenneskens@gmail.com>
2020-08-12 14:57:38 -0500

    finished

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -20,7 +20,18 @@ with open(election_datacsv) as csvfile:
     for row in csvreader:
         polls.append(row)
 
-    print(str(len(polls)))
+    # print summary
+    print("Election Results")
+    print("------------------------------------") 
+    print("Total Votes: "+str(len(polls)))
+    print("------------------------------------")
+
+    # write to text file
+    text_file = open(output_txt, "w")
+    text_file.write("Election Results")
+    text_file.write("\n------------------------------------")
+    text_file.write("\nTotal Votes: " + str(len(polls)))
+    text_file.write("\n------------------------------------")
 
     # convert polls list in dictionary for counting names of candidate
     for row in polls:
@@ -35,13 +46,27 @@ with open(election_datacsv) as csvfile:
     total_polls = len(polls)
     for name in dic_names:
         dic_votes[name] = round((dic_names[name] / total_polls) * 100)
+
+        # print percentage and votes per candidate
         print(str(name) + ": " + str(dic_votes[name]) + "% " + "(" + str(dic_names[name]) + ")")
 
+        # write percentage and votes per candidate to text file
+        text_file.write("\n" + str(name) + ": " + str(dic_votes[name]) + "% " + "(" + str(dic_names[name]) + ")")
+
     # set variable to compare and define winner
-    highest=0
+    highest = 0
     for name in dic_votes:
         if highest < dic_votes[name]:
             highest = dic_votes[name]
             winner = name
 
-    print(winner)
+    # print winner
+    print("------------------------------------")
+    print("Winner: " + winner)
+    print("------------------------------------")
+
+    # write winner to text file
+    text_file.write("\n------------------------------------")
+    text_file.write("\nWinner: " + winner)
+    text_file.write("\n------------------------------------")
+    text_file.close()
