8c7b37c664ee7ec0e57fd8cb720e2a8dea651b02
Jimi <jimijenneskens@gmail.com>
2020-08-12 13:11:26 -0500

    summaryadded

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -21,3 +21,18 @@ with open(election_datacsv) as csvfile:
         polls.append(row)
 
     print(str(len(polls)))
+
+    # convert polls list in dictionary for counting names of candidate
+    for row in polls:
+        candidate = row[2]
+        if candidate not in dic_names:
+           # put name in dictionary 
+            dic_names[candidate]=0
+        # cout name in dictionary
+        dic_names[candidate]+=1
+
+    # define names, percentage and number of votes for each candidate
+    total_polls = len(polls)
+    for name in dic_names:
+        dic_votes[name] = round((dic_names[name] / total_polls) * 100)
+        print(str(name) + ": " + str(dic_votes[name]) + "% " + "(" + str(dic_names[name]) + ")")
